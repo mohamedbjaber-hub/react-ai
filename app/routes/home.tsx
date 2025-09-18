0323932af5838d920c2ef58194d6f2a247ce5203
ibramonde <ibramonde@gmail.com>
2025-09-18 13:22:46 +0200

    config authentification avec puter.js et puter.com

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,13 +14,19 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth } = usePuterStore();
+    const navigate = useNavigate();
+    useEffect(() => {
+        console.log('auth 2', auth.isAuthenticated)
+        if(!auth.isAuthenticated) navigate('/auth?next=/');
+    },[auth.isAuthenticated])
   return (
     <main className="bg-[url('./images/bg-main.svg')] bg-cover">
       <Navbar />
       <section className="main-section">
         <div className="page-heading py-6">
           <h1>Tester votre CV</h1>
-          <h2>Revoir votre condidature et verfier votre CV avec AI </h2>
+          <h2>Valider votre condidature et verfier votre CV avec AI </h2>
         </div>
         {resumes.length > 0 && (
           <div className="resumes-section">
